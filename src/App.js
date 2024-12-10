72b07ff9bb32e806c518a556432854495fe40c92
Diego Damian <dad405@scarletmail.rutgers.edu>
2024-12-10 13:31:21 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Timer from './Timer'; // Import the Timer component
 import './App.css';
+import Report from "./Report"; // Import CSS for styling
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [view, setView] = useState('Timer'); // "Timer" or "Report"
+
+    return (
+        <div className="App">
+            <header className="header">
+                <h1>Diego's Pomodoro</h1>
+                <div className="nav-buttons">
+                    <button onClick={() => setView('Timer')}>Timer</button>
+                    <button onClick={() => setView('Report')}>Activity Log</button>
+                </div>
+            </header>
+            <main className="content">
+            {view === 'Timer' ? <Timer />  : <Report />}
+            </main>
+
+        </div>
+    );
 }
 
 export default App;
